b4dde874377be837e68c2d512d569776328ad662
Bruno Tavares <bruno.exz@gmail.com>
2012-02-16 23:02:37 -0200

    Install Script created

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+curr_dir=`pwd`
+
+
+#Set up vim
+[[ -d $HOME/.vim ]] && mv $HOME/.vim $HOME/.vim-bkp 
+ln -s $curr_dir/vim $HOME/.vim
+
+[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc-bkp
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+
+#gnome-terminal
+gconftool-2 --load $curr_dir/gnome-terminal/config.xml 
+
+#Zshell
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+mv $HOME/.oh-my-zsh/custom $HOME/.oh-my-zsh/custom-bkp
+mv $HOME/.zshrc $HOME/.zshrc-bkp
+ln -s $curr_dir/zsh/zshrc $HOME/.zshrc
+ln -s $curr_dir/zsh/oh-my-zsh/custom $HOME/.oh-my-zsh/custom
+chsh -s /bin/zsh
+
+
+#tmux
+[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/.tmux.conf-bkp
+ln -s $curr_dir/tmux.conf $HOME/.tmux.conf
