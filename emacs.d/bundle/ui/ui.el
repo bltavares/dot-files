f6b8c17592fd2f8405b8ece6fb6c0153d9a6ca20
Bruno Tavares <bruno.exz@gmail.com>
2012-11-24 00:59:03 -0200

    Added emacs configurations

---
+++ emacs.d/bundle/ui/ui.el
@@ -0,0 +1,5 @@
+(menu-bar-showhide-tool-bar-menu-customize-disable)
+(setq inhibit-splash-screen t)
+(scroll-bar-mode -1)
+
+(provide 'ui)
---
+++ emacs.d/init.el
@@ -0,0 +1,13 @@
+(let ((default-directory "~/.emacs.d/bundle/"))
+  (dolist (f (directory-files default-directory))
+    (when (and (not (equal f ".")) (not (equal f "..")))
+      (add-to-list 'load-path (concat default-directory f))
+      (require (intern f))
+      )))
+
+(let ((themes-directory "~/.emacs.d/themes"))
+  (when (file-directory-p themes-directory) 
+    (dolist (f (directory-files themes-directory))
+      (when (and (not (equal f ".")) (not (equal f "..")))
+        (add-to-list 'custom-theme-load-path f)
+        ))))
