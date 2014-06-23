3aa212dac0ff1928e7657af053a512c2ac754e09
Bruno Tavares <connect+github@bltavares.com>
2014-06-22 23:14:52 -0300

    Change the default terminal

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -45,7 +45,7 @@ end
 beautiful.init(awful.util.getdir('config') .. "/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
