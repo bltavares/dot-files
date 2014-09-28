4fcb83fdf909dee6d69185aa845d31886c0374ee
Bruno Tavares <bruno.exz@gmail.com>
2014-09-27 21:48:29 -0300

    Cleanup the install instructions and add the installation of the package managers for the editors

--- install.sh
+++ install.sh
@@ -1,59 +1,76 @@
 #!/bin/bash
 
-#Initialize submodules
-git submodule init; git submodule update;
-
 curr_dir=`pwd`
 
+linking() {
+  rm -r $2 2>/dev/null || true
+  ln -s $1 $2
+}
 
-#Set up vim
-[[ -d $HOME/.vim ]] && mv $HOME/.vim $HOME/.vim-bkp 
-ln -s $curr_dir/vim $HOME/.vim
+install.vim() {
+  mkdir -p vim/autoload
+  curl -fLo vim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc-bkp
-ln -s $HOME/.vim/vimrc $HOME/.vimrc
+  linking $curr_dir/vim $HOME/.vim
+  linking $curr_dir/vim/vimrc $HOME/.vimrc
+}
 
-#Set up emacs
-[[ -d $HOME/.emacs.d ]] && mv $HOME/.emacs.d $HOME/.emacs.d-bkp
-ln -s $curr_dir/emacs.d $HOME/.emacs.d
+install.emacs() {
+  [[ -d $HOME/.cask ]] || git clone https://github.com/cask/cask.git $HOME/.cask
+  linking $curr_dir/emacs.d $HOME/.emacs.d
+}
 
-[[ -d $HOME/.gitignore_global ]] && mv $HOME/.gitignore_global $HOME/.gitignore_global-bkp
-ln -s $curr_dir/gitignore_global $HOME/.gitignore_global
+install.confs() {
+  linking $curr_dir/gitignore_global $HOME/.gitignore_global
+  linking $curr_dir/tmux.conf $HOME/.tmux.conf
+  linking $curr_dir/ctags $HOME/.ctags
 
-#gnome-terminal
-which gconftool-2 && gconftool-2 --load $curr_dir/gnome-terminal/config.xml 
+  mkdir -p $HOME/.config
+  linking $curr_dir/awesome $HOME/.config/awesome
+}
 
-#tmux
-[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/.tmux.conf-bkp
-ln -s $curr_dir/tmux.conf $HOME/.tmux.conf
+install.zsh() {
+  [[ -d $HOME/.oh-my-zsh ]] || git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+  linking $curr_dir/zsh/zshrc $HOME/.zshrc
+  linking $curr_dir/zsh/oh-my-zsh/custom $HOME/.oh-my-zsh/custom
+}
 
-#ctags
-[[ -f $HOME/.ctags ]] && mv $HOME/.ctags $HOME/.ctags-bkp
-ln -s $curr_dir/ctags $HOME/.ctags
+install.bins() {
+  mkdir -p $HOME/bin
+  for b in $curr_dir/bin/*; do
+    linking $b $HOME/bin/`basename $b`
+  done
+}
 
-#Zshell
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-mv $HOME/.oh-my-zsh/custom $HOME/.oh-my-zsh/custom-bkp
-mv $HOME/.zshrc $HOME/.zshrc-bkp
-ln -s $curr_dir/zsh/zshrc $HOME/.zshrc
-ln -s $curr_dir/zsh/oh-my-zsh/custom $HOME/.oh-my-zsh/custom
+#OS=`uname -s`
+#if [[ "$OS" != "Darwin" ]]; then
+  #cd $HOME
+  #curl -L https://gist.github.com/bltavares/2706792/raw/post-install.sh | bash
+#fi
 
-#Awesome
-mkdir -p $HOME/.config
-[[ -d $HOME/.config/awesome ]] && mv $HOME/.config/awesome $HOME/.config/awesome-bkp 
-ln -s $curr_dir/awesome $HOME/.config/awesome
+install.all() {
+  install.bins
+  install.emacs
+  install.vim
+  install.confs
+  install.zsh
+}
 
-#Custom commands
-mkdir -p $HOME/bin
-for b in $curr_dir/bin/*; do
-  ln -s $b $HOME/bin/`basename $b`
-done
+options=("bins" "emacs" "vim" "confs" "zsh" "all")
+help() {
+  echo "install.sh [option]"
+  echo "[options] - one of the following:"
+  for option in ${options[*]}; do
+    echo " - $option"
+  done
+}
 
-OS=`uname -s`
-if [[ "$OS" != "Darwin" ]]; then
-  cd $HOME
-  curl -L https://gist.github.com/bltavares/2706792/raw/post-install.sh | bash
+command=$1; shift
+if [[ "${options[*]}" =~ "${command:-not-found}" ]]; then
+ install.$command "$@"
+else
+ help
 fi
-
 
 touch $HOME/.baseline_dotfiles
