eeefcad4dd178eb43bcd871594fca057490ae4e2
Bruno Tavares <bruno.exz@gmail.com>
2014-10-02 15:13:37 -0300

    Add ido, with filename guessing under cursor

--- emacs.d/config/config.el
+++ emacs.d/config/config.el
@@ -6,6 +6,10 @@
 (add-hook 'clojure-mode-hook 'subword-mode)
 (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
 
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-filename-at-point 'guess)
+
 ;;Mac only
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
