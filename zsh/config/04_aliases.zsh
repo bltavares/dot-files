602d8a41517863d7324ef8a4346188da910436eb
Bruno Tavares <connect+github@bltavares.com>
2016-04-29 13:51:26 -0300

    Removes alias on hub. Hub should be used explicitly.

--- zsh/config/04_aliases.zsh
+++ zsh/config/04_aliases.zsh
@@ -2,10 +2,4 @@ alias tw='t timeline -r'
 alias pw='password'
 alias be='bundle exec'
 alias minecraft='java -Xmx1660M -Xms1660M -jar ~/minecraft.jar'
-
-if which hub > /dev/null; then
-  alias g='hub'
-  eval "$(hub alias -s)"
-else
-  alias g='git'
-fi
+alias g='git'
