0e8de07b98a3693d31960eeeb59d6baac1809b0e
Bruno Tavares <connect+github@bltavares.com>
2016-08-31 17:06:21 -0300

    Use hs instead of select

--- zsh/config/04_functions.zsh
+++ zsh/config/04_functions.zsh
@@ -11,18 +11,19 @@ camelize() {
 }
 
 switch-branch() {
-  git branch | cut -c 3- | selecta | xargs git checkout
+  git branch | cut -c 3- | hs | xargs git checkout
 }
 
 checkout-branch() {
   if [ -z "$1" ] || git remote | grep -q $1; then
     branch=${1:-origin}
-    git branch -r | grep "$branch/" | cut -d'/' -f 2- | selecta | xargs -I{} git checkout -b '{}' $branch/'{}'
+    git branch -r | grep "$branch/" | cut -d'/' -f 2- | hs | xargs -I{} git checkout -b '{}' $branch/'{}'
   else
     git checkout -b "$1" origin/"$1"
   fi
 }
-p() { cd $(find ~/Projects ~/repos -type d -maxdepth 1 | selecta) }
+
+p() { cd $(find ~/Projects/* ~/repos -type d -maxdepth 1 | hs) }
 
 function say {
   lang=${2:-en}
