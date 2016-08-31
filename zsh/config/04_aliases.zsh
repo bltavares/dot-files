a9d154d2e05a4fd68017f98b1c7cf6b03d81f54f
Bruno Tavares <connect+github@bltavares.com>
2016-08-31 17:03:38 -0300

    Add more commands to manage pocket
    This commit removes the alias and move then to commands, where we have a
    whole file to configure more commands.
    
    We introduced many other commands, where we can now manage PDFs and
    YouTube videos as well.

--- zsh/config/04_aliases.zsh
+++ zsh/config/04_aliases.zsh
@@ -3,6 +3,3 @@ alias pw='password'
 alias be='bundle exec'
 alias minecraft='java -Xmx1660M -Xms1660M -jar ~/minecraft.jar'
 alias g='git'
-alias pocket-star="pickpocket-batch-favorite ~/star"
-alias pocket-add="pickpocket-batch-add ~/add"
-alias pocket-read="pickpocket-batch-read ~/read"
