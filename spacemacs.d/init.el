617eb9e8a69912c5448210461f8b399d1ddc8f64
Bruno Tavares <connect+github@bltavares.com>
2017-11-03 10:16:16 -0200

    Bump spacemacs config version to the RC 0.300

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -9,6 +9,8 @@ You should not put any user code in this function besides modifying the variable
 values."
   (setq-default
    dotspacemacs-distribution 'spacemacs
+   dotspacemacs-enable-lazy-installation 'unused
+   dotspacemacs-ask-for-lazy-installation t
    dotspacemacs-configuration-layer-path '()
    dotspacemacs-configuration-layers
    '(
@@ -19,6 +21,8 @@ values."
      ;; ----------------------------------------------------------------
      better-defaults
      syntax-checking
+     ;; Parinfer is too buggy on this codebases
+     ;; parinfer
      (spell-checking :variables
                      spell-checking-enable-auto-dictionary t
                      enable-flyspell-auto-completion t)
@@ -66,73 +70,63 @@ values."
    dotspacemacs-excluded-packages '()
    dotspacemacs-install-packages 'used-only)
 
-   (when (dotspacemacs/system-is-mac)
-     (push 'osx dotspacemacs-configuration-layers)))
+  (when (dotspacemacs/system-is-mac)
+    (push 'osx dotspacemacs-configuration-layers)))
 
 (defun dotspacemacs/init ()
-  "Initialization function.
-This function is called at the very startup of Spacemacs initialization
-before layers configuration.
-You should not put any user code in there besides modifying the variable
-values."
+  "Initialization:
+This function is called at the very beginning of Spacemacs startup,
+before layer configuration.
+It should only modify the values of Spacemacs settings."
+  ;; This setq-default sexp is an exhaustive list of all the supported
+  ;; spacemacs settings.
   (setq-default
    ;; Updates
    dotspacemacs-elpa-https t
    dotspacemacs-elpa-timeout 5
+   dotspacemacs-verify-spacelpa-archives nil
    dotspacemacs-check-for-update nil
+   dotspacemacs-elpa-subdirectory 'emacs-version
+   dotspacemacs-editing-style 'vim
    dotspacemacs-verbose-loading nil
-   dotspacemacs-loading-progress-bar t
 
    ;; Startup
    dotspacemacs-startup-banner 'official
    dotspacemacs-startup-lists nil
    dotspacemacs-startup-recent-list-size 5
-   dotspacemacs-scratch-mode 'text-mode
    dotspacemacs-startup-buffer-responsive t
-
-   ;; UI
-   dotspacemacs-active-transparency 90
-   dotspacemacs-colorize-cursor-according-to-state t
-   dotspacemacs-fullscreen-at-startup nil
-   dotspacemacs-fullscreen-use-non-native nil
-   dotspacemacs-inactive-transparency 90
-   dotspacemacs-line-numbers 'relative
-   dotspacemacs-maximized-at-startup nil
-   dotspacemacs-mode-line-unicode-symbols (if (dotspacemacs/system-is-mac) t nil)
-   dotspacemacs-smooth-scrolling t
+   dotspacemacs-scratch-mode 'text-mode
    dotspacemacs-themes '(sanityinc-tomorrow-night
                          spacemacs-dark
                          sanityinc-tomorrow-day
+                         spacemacs-dark
                          spacemacs-light)
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
+   dotspacemacs-colorize-cursor-according-to-state t
+   dotspacemacs-default-font '("Monaco"
+                               :size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
 
    ;; Experience
-   dotspacemacs-editing-style 'vim
    dotspacemacs-leader-key "SPC"
+   dotspacemacs-emacs-command-key "SPC"
+   dotspacemacs-ex-command-key ":"
    dotspacemacs-emacs-leader-key "M-m"
    dotspacemacs-major-mode-leader-key ","
    dotspacemacs-major-mode-emacs-leader-key "C-M-m"
    dotspacemacs-distinguish-gui-tab nil
+   dotspacemacs-remap-Y-to-y$ t
    dotspacemacs-retain-visual-state-on-shift t
    dotspacemacs-visual-line-move-text nil
    dotspacemacs-ex-substitute-global nil
-   dotspacemacs-command-key ":"
-   dotspacemacs-remap-Y-to-y$ t
-   dotspacemacs-enable-paste-micro-state nil
-   dotspacemacs-which-key-delay 0.4
-   dotspacemacs-which-key-position 'bottom
-   dotspacemacs-smartparens-strict-mode nil
-   dotspacemacs-smart-closing-parenthesis nil
 
    ;; Layout
    dotspacemacs-default-layout-name "Default"
    dotspacemacs-display-default-layout nil
    dotspacemacs-auto-resume-layouts nil
-
+   dotspacemacs-auto-generate-layout-names nil
+   dotspacemacs-large-file-size 1
    dotspacemacs-auto-save-file-location 'cache
    dotspacemacs-max-rollback-slots 1
 
@@ -144,33 +138,49 @@ values."
 
    ;; Others
    dotspacemacs-enable-paste-transient-state nil
+   dotspacemacs-which-key-delay 0.4
+   dotspacemacs-which-key-position 'bottom
+   dotspacemacs-switch-to-buffer-prefers-purpose nil
+   dotspacemacs-loading-progress-bar t
+   dotspacemacs-fullscreen-at-startup nil
+   dotspacemacs-fullscreen-use-non-native nil
+   dotspacemacs-maximized-at-startup nil
+   dotspacemacs-active-transparency 90
+   dotspacemacs-inactive-transparency 90
+   dotspacemacs-show-transient-state-title t
    dotspacemacs-show-transient-state-color-guide t
+   dotspacemacs-mode-line-unicode-symbols (if (dotspacemacs/system-is-mac) t nil)
    dotspacemacs-smooth-scrolling t
+   dotspacemacs-line-numbers 'relative
+   dotspacemacs-folding-method 'evil
+   dotspacemacs-smartparens-strict-mode nil
+   dotspacemacs-smart-closing-parenthesis nil
 
-   ;; TODO Search this
    dotspacemacs-highlight-delimiters 'all
    dotspacemacs-persistent-server nil
    dotspacemacs-search-tools '("rg" "ag" "pt" "ack" "grep")
    dotspacemacs-default-package-repository nil
+   dotspacemacs-frame-title-format "%I@%S"
+   dotspacemacs-icon-title-format nil
    dotspacemacs-whitespace-cleanup 'trailing
+   dotspacemacs-zone-out-when-idle nil
+   dotspacemacs-pretty-docs nil
    ))
 
 (defun dotspacemacs/user-init ()
-  "Initialization function for user code.
-It is called immediately after `dotspacemacs/init', before layer configuration
-executes.
- This function is mostly useful for variables that need to be set
-before packages are loaded. If you are unsure, you should try in setting them in
-`dotspacemacs/user-config' first."
+  "Initialization for user code:
+This function is called immediately after `dotspacemacs/init', before layer
+configuration.
+It is mostly for variables that should be set before packages are loaded.
+If you are unsure, try setting them in `dotspacemacs/user-config' first."
   )
 
 (defun dotspacemacs/user-config ()
-  "Configuration function for user code.
-This function is called at the very end of Spacemacs initialization after
-layers configuration.
-This is the place where most of your configurations should be done. Unless it is
-explicitly specified that a variable should be set before a package is loaded,
-you should place your code here."
+  "Configuration for user code:
+This function is called at the very end of Spacemacs startup, after layer
+configuration.
+Put your configuration code here, except for variables that should be set
+before packages are loaded."
   (setq powerline-default-separator nil)
   (spaceline-compile)
   (fset 'evil-visual-update-x-selection 'ignore)
