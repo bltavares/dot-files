bcada4eb3cfdd22f9b4bc16ac7d98e7977410be1
Bruno Tavares <connect+github@bltavares.com>
2021-04-20 11:14:46 -0300

    Neovim lua support

---
+++ nvim/lua/04_lang_lua.lua
@@ -0,0 +1,33 @@
+local on_attach = require 'utils.lsp.on_attach';
+local capabilities = require'utils.lsp.capabilities'.create()
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {'lua-language-server'};
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+      -- Do not send telemetry ata containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
