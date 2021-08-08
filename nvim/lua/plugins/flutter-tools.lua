bfcf129d4013c1914bc391026309ba2e773e7dd6
Bruno Tavares <connect+github@bltavares.com>
2021-08-08 19:56:56 -0300

    Move fix from flutter into general to also fix lua lsp loading

--- nvim/lua/plugins/flutter-tools.lua
+++ nvim/lua/plugins/flutter-tools.lua
@@ -67,12 +67,3 @@ require("flutter-tools").setup {
     widget_guides = {enabled = true},
     lsp = {on_attach = on_attach, capabilities = capabilities}
 }
-
--- In order for neovim to launch certain executables on Windows, it must append .cmd to the command name. A fix is in the works upstream, but until this is mainlined please the following somewhere in your init.vim (lua heredoc) or init.lua:
-vim.loop.spawn = (function()
-    local spawn = vim.loop.spawn
-    return function(path, options, on_exit)
-        local full_path = vim.fn.exepath(path)
-        return spawn(full_path, options, on_exit)
-    end
-end)()
