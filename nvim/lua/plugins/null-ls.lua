54d6d0f2886690fcc77a93540a562ffe207713c9
Bruno Tavares <connect+github@bltavares.com>
2021-11-16 15:43:57 -0300

    Fix typo

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -3,7 +3,7 @@ local null_ls = require("null-ls")
 null_ls.config({
     sources = {
         null_ls.builtins.formatting.shfmt,
-        null_ls:builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.shellcheck,
     }
 })
 
