77ee61d5661b09c5ea0a8ed65e13e4135b54be51
Bruno Tavares <connect+github@bltavares.com>
2023-07-29 15:38:52 -0300

    Add go lsp

---
+++ nvim/lua/plugins/lsp/lang/go.lua
@@ -0,0 +1,8 @@
+local on_attach = require 'plugins.lsp.on_attach';
+local capabilities = require'plugins.lsp.capabilities'.create()
+
+-- TODO syntax highlight with treesitter
+require'lspconfig'.gopls.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
