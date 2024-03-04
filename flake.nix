56c907b898bd7d83d826d29f0fe93eca83df24bd
Bruno Tavares <connect+github@bltavares.com>
2024-03-04 18:26:41 -0300

    nix: support m1 config

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
       imports = [
         inputs.nixos-flake.flakeModule
         inputs.treefmt-nix.flakeModule
