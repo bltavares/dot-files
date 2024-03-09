bb9689a77583f0800e7dd8296587feab2ec0f277
Bruno Tavares <connect+github@bltavares.com>
2024-03-09 17:20:53 -0300

    nvim: Add nix lsp support

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -2,6 +2,7 @@ require("config.lsp.lang.clojure")
 require("config.lsp.lang.lua")
 require("config.lsp.lang.terraform")
 require("config.lsp.lang.go")
+require("config.lsp.lang.nix")
 -- require("config.lsp.lang.kubernetes")
 -- incomplete lsp
 -- require 'config.lsp.lang.nomad'
