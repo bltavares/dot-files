1cd3da909f7f425dff9f2cc3812cc4c934778589
Bruno Tavares <connect+github@bltavares.com>
2024-04-04 11:27:51 -0300

    Enable Garnix on repo to cache devenv

--- nix/os/modules/nix.nix
+++ nix/os/modules/nix.nix
@@ -7,9 +7,11 @@
       experimental-features = ["nix-command" "flakes"];
       extra-substituters = [
         "https://nix-community.cachix.org"
+        "https://cache.garnix.io"
       ];
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       bash-prompt-prefix = "($name) ";
 
