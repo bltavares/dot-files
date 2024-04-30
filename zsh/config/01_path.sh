e2f6ac22ecf5c9220639253685603d1cd10301a2
Bruno Tavares <connect+github@bltavares.com>
2024-04-30 11:06:01 -0300

    brew: include brew path on macos

--- zsh/config/01_path.sh
+++ zsh/config/01_path.sh
@@ -3,3 +3,7 @@ export PATH="$HOME/bin:$HOME/.cask/bin:$PATH"
 if [[ -e /home/linuxbrew ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
+if [[ -e /opt/homebrew/bin ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
