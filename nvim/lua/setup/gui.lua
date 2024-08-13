b43ed41b6ab1193cdc12087875ac68fee64ce6bd
Bruno Tavares <connect+github@bltavares.com>
2024-08-13 14:37:12 -0300

    nvim: Migrate neovide argument for macos

--- nvim/lua/setup/gui.lua
+++ nvim/lua/setup/gui.lua
@@ -23,5 +23,5 @@ if vim.g.neovide then
     vim.o.guifont = font .. ":h" .. font_size
   end
 
-  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_input_macos_option_key_is_meta = true
 end
