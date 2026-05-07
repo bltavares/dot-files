acc0e4120843f7e1f90efc9135a63ca91f5ad5bc
Bruno Tavares <connect+github@bltavares.com>
2026-05-07 14:12:14 -0300

    (starship) Restore custom hostname colors through env vars

--- zsh/config/03_ps1.sh
+++ zsh/config/03_ps1.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 ## Set PS1
+
+function colored_ps1() {
+  if [[ -n "$X_COLOR_PS1" ]]; then
+    echo "$X_COLOR_PS1"
+    return
+  fi
+
+  local OS=$(uname -s)
+  if [[ "Darwin" == "$OS" ]]; then
+    local MD5="md5"
+  else
+    local MD5="md5sum"
+  fi
+
+  local host_num=$((0x$(hostname | $MD5 | cut -c1-8) % 216 + 1))
+  local user_num=$((0x$(whoami | $MD5 | cut -c1-8) % 216 + 1))
+  print -P "%{$FG[$user_num]%}%n%{$FG[$host_num]%}@%m%{$reset_color%}"
+}
+
 if which starship >/dev/null 2>&1; then
+  export X_COLOR_PS1="$(colored_ps1)"
   eval "$(starship init zsh)"
 fi
 
