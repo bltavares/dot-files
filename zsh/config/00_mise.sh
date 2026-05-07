1673f5517636909082781474d16daa571efca7b1
Bruno Tavares <connect+github@bltavares.com>
2026-05-07 14:14:33 -0300

    (zsh) Reformat files with shfmt

--- zsh/config/00_mise.sh
+++ zsh/config/00_mise.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
 ## Setup global host config
-export HOST="$(hostname | tr '[:upper:]' '[:lower:]')"
-export MISE_SYSTEM_CONFIG_DIR="${HOME}/.config/mise/hosts/${HOST}"
-if [[ -z "$X_MISE_GLOBAL" && -d "$MISE_SYSTEM_CONFIG_DIR" ]]; then
-  which mise > /dev/null && \
-    X_MISE_GLOBAL=on exec mise en
-fi
+(
+	TARGET="$(hostname | tr '[:upper:]' '[:lower:]')"
+	export MISE_SYSTEM_CONFIG_DIR="${HOME}/.config/mise/hosts/${TARGET}"
+	if [[ -z "$X_MISE_GLOBAL" && -d "$MISE_SYSTEM_CONFIG_DIR" ]]; then
+		which mise >/dev/null &&
+			X_MISE_GLOBAL=on exec mise en
+	fi
+)
 
 x() {
-  if [[ $# -eq 0 ]]; then
-    local local_mise="$(mise cfg ls --silent --no-header | grep -v "~/.config/mise" | wc -l )"
-    printf "Available envs:\n\n"
-    if [[ "$local_mise" -gt 0 ]]; then
-      echo "(local)"
-    fi
-    for f in ~/.config/mise/mise.*.toml; do
-      basename "${f//mise./}" .toml
-    done
-    return
-  fi
+	if [[ $# -eq 0 ]]; then
+		local local_mise="$(mise cfg ls --silent --no-header | grep -v "~/.config/mise" | wc -l)"
+		printf "Available envs:\n\n"
+		if [[ "$local_mise" -gt 0 ]]; then
+			echo "(local)"
+		fi
+		for f in ~/.config/mise/mise.*.toml; do
+			basename "${f//mise./}" .toml
+		done
+		return
+	fi
 
-  if [[ -z "$MISE_ENV" ]]; then
-    declare -a MISE_ENV=( "$@" )
-  else
-    IFS="," read -r -A MISE_ENV <<<"$MISE_ENV"
-    MISE_ENV+=( "$@" )
-  fi
+	if [[ -z "$MISE_ENV" ]]; then
+		declare -a MISE_ENV=("$@")
+	else
+		IFS="," read -r -A MISE_ENV <<<"$MISE_ENV"
+		MISE_ENV+=("$@")
+	fi
 
-  MISE_ENV="${MISE_ENV[*]// /,}" MISE_SHELL=zsh mise en
+	MISE_ENV="${MISE_ENV[*]// /,}" MISE_SHELL=zsh mise en
 }
 
 xo() {
-  if [[ $# -eq 0 ]]; then
-    printf "Available envs:\n\n"
-    for f in ~/.config/mise/mise.*.toml; do
-      basename "${f//mise./}" .toml
-    done
-    return
-  fi
+	if [[ $# -eq 0 ]]; then
+		printf "Available envs:\n\n"
+		for f in ~/.config/mise/mise.*.toml; do
+			basename "${f//mise./}" .toml
+		done
+		return
+	fi
 
-  cp ~/.config/mise/mise.$1.toml mise.local.toml
+	cp ~/.config/mise/mise.$1.toml mise.local.toml
 }
-
--- zsh/config/03_ps1.sh
+++ zsh/config/03_ps1.sh
@@ -3,26 +3,26 @@
 ## Set PS1
 
 function colored_ps1() {
-  if [[ -n "$X_COLOR_PS1" ]]; then
-    echo "$X_COLOR_PS1"
-    return
-  fi
+	if [[ -n "$X_COLOR_PS1" ]]; then
+		echo "$X_COLOR_PS1"
+		return
+	fi
 
-  local OS=$(uname -s)
-  if [[ "Darwin" == "$OS" ]]; then
-    local MD5="md5"
-  else
-    local MD5="md5sum"
-  fi
+	local OS=$(uname -s)
+	if [[ "Darwin" == "$OS" ]]; then
+		local MD5="md5"
+	else
+		local MD5="md5sum"
+	fi
 
-  local host_num=$((0x$(hostname | $MD5 | cut -c1-8) % 216 + 1))
-  local user_num=$((0x$(whoami | $MD5 | cut -c1-8) % 216 + 1))
-  print -P "%{$FG[$user_num]%}%n%{$FG[$host_num]%}@%m%{$reset_color%}"
+	local host_num=$((0x$(hostname | $MD5 | cut -c1-8) % 216 + 1))
+	local user_num=$((0x$(whoami | $MD5 | cut -c1-8) % 216 + 1))
+	print -P "%{$FG[$user_num]%}%n%{$FG[$host_num]%}@%m%{$reset_color%}"
 }
 
 if which starship >/dev/null 2>&1; then
-  export X_COLOR_PS1="$(colored_ps1)"
-  eval "$(starship init zsh)"
+	export X_COLOR_PS1="$(colored_ps1)"
+	eval "$(starship init zsh)"
 fi
 
 ## Make non-break character clear on paste
@@ -31,17 +31,17 @@ bindkey -s $nbsp '^u'
 
 ## Move line up on terminal after submit
 function preexec {
-  if [[ -z "$NO_AUTOTOP" ]]; then
-    tput cuu1
-    tput el
-    clear
-    print -Pn $PS1
-    print $1
-  fi
+	if [[ -z "$NO_AUTOTOP" ]]; then
+		tput cuu1
+		tput el
+		clear
+		print -Pn $PS1
+		print $1
+	fi
 }
 
 function precmd {
-  if [[ -z "$NO_AUTOTOP" ]]; then
-    tput cup $(($(tput lines))) 0
-  fi
+	if [[ -z "$NO_AUTOTOP" ]]; then
+		tput cup $(($(tput lines))) 0
+	fi
 }
