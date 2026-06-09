70602d3f9dd2dd34a0d5edd7d9a0bdeb692ea576
Bruno Tavares <connect+github@bltavares.com>
2026-06-09 19:15:02 +0000

    nvim: add toml and nginx treesitter syntaxes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -15,7 +15,9 @@ return {
         "rust",
         "yaml",
         "json",
+        "toml",
         "ini",
+        "nginx",
       }
       vim.defer_fn(function()
         require("nvim-treesitter").install(ensure_installed)
