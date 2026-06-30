d3637c40f0b7c7af737cb32faeea05cd30f28576
Bruno Tavares <connect+github@bltavares.com>
2026-06-30 16:14:31 -0300

    nvim: fork md-preview to avoid error notification on windows

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,9 @@
 return {
   {
-    "topazape/md-preview.nvim",
+    --"topazape/md-preview.nvim",
+    -- HACK: https://github.com/topazape/md-preview.nvim/pull/1
+    "bltavares/md-preview.nvim",
+    branch = 'fix-win-11-support',
     cmd = { "MPToggle" },
     ft = "markdown",
     keys = {
