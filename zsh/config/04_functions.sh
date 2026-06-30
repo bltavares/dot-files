84a26c8784cad49dafe71193b3386b6a5f584009
Bruno Tavares <connect+github@bltavares.com>
2026-06-30 16:32:03 -0300

    fix: run dos2unix on all files

--- zsh/config/04_functions.sh
+++ zsh/config/04_functions.sh
@@ -21,11 +21,11 @@ checkout-branch() {
 p() { cd $(find ~/dev/* ~/repos -type d -maxdepth 1 | hs); }
 
 ssh-login() {
-	(
-		set -e
-		ssh-add -D
-		opkssh login --provider https://id.bltavares.com/auth/v1/,opkssh
-		ssh-add
-		opkssh logout
-	)
+  (
+    set -e
+    ssh-add -D
+    opkssh login --provider https://id.bltavares.com/auth/v1/,opkssh
+    ssh-add
+    opkssh logout
+  )
 }
